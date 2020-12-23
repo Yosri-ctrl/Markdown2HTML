86d1a386a835d8660db2e99b00dae65a5577d22a
Yosri-ctrl <yosribouabid@gmail.com>
2020-12-23 16:10:01 +0100

    task 1

--- markdown2html.py
+++ markdown2html.py
@@ -3,12 +3,34 @@
 from sys import argv, stderr, exit
 from os import path
 
-if __name__ == "__main__":
+
+def checker():
     if len(argv) <= 2:
         stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        exit(1)
+        return(1)
 
     if not path.exists(argv[1]):
         stderr.write("Missing {}\n".format(argv[1]))
+        return(1)
+    return(0)
+
+
+def converter():
+    count = 0
+    with open(argv[1], "r") as r:
+        with open(argv[2], "w") as w:
+            for line in r.readlines():
+                for x in line:
+                    if x == "#":
+                        count += 1
+                if (count != 0):
+                    w.write(
+                        " <h{}> Heading level {}</h{}>\n".format(count, count, count))
+                count = 0
+
+
+if __name__ == "__main__":
+    if checker():
         exit(1)
+    converter()
     exit(0)
