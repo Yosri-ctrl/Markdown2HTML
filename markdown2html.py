5fa315b6b735dcc9b39608ca6c0b77744559d9d7
Yurei <yosribouabid@gmail.com>
2021-03-15 03:41:40 +0100

    task 2

--- markdown2html.py
+++ markdown2html.py
@@ -16,30 +16,35 @@ def checker():
 
 
 def converter():
-    count=0
+    counth = 0
+    countl = 0
+    listing = []
     with open(argv[1], "r") as r:
-        with open(argv[2], "w") as w:
-
+        with open(argv[2], "w+") as w:
             for line in r.readlines():
                 for char in line:
-                    if char == "#": count+=1
-                    
+                    if char == "#": counth += 1
+
                 if line[0] == "#":
-                    w.write("<h"+str(count)+">"+ line[count+1:-1] +"</h"+str(count)+">\n")
-                count=0
-    
-    """count = 0
+                    w.write("<h"+str(counth)+">"+ line[counth+1:-1] +"</h"+str(counth)+">\n")
+                if line[0] == "-":
+                    listing.append(line[2:-1])
+                    countl = 1
+                counth = 0
+    if(countl): 
+        list(listing)
+
+
+def list(listing):
     with open(argv[1], "r") as r:
-        with open(argv[2], "w") as w:
+        with open(argv[2], "a") as w:
             for line in r.readlines():
-                for x in line:
-                    if x == "#":
-                        count += 1
-                if (count != 0):
-                    w.write(
-                        " <h{}> Heading level {}</h{}>\n".format(count, count, count))
-                count = 0
-"""
+                if line[0] == "-":
+                    w.write("<ul>\n")
+                    for l in listing:
+                        w.write("   <li>"+ l +"</li>\n")
+                    w.write("</ul>\n")
+                    break
 
 if __name__ == "__main__":
     if checker():
